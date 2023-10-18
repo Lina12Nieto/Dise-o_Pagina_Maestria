b94cefa4575d2cc63a573d847855e4269c0af5f3
Lina Maria Nieto Alarcon <lina.nieto22243@ucaldas.edu.co>
2023-10-18 00:06:05 -0500

    Creacion del ancabezado y el pie de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './publico/pagina-maestra/encabezado/encabezado.component';
+import { PiePaginaComponent } from './publico/pagina-maestra/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
