19ee1c6d16e5589760b5d86070e6d8ec632caba1
Lina Maria Nieto Alarcon <lina.nieto22243@ucaldas.edu.co>
2023-10-26 22:15:58 -0500

    Creacion paginas y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './publico/home/home.component';
+import { ContactoComponent } from './publico/contacto/contacto.component';
+import { ObjetivosComponent } from './publico/objetivos/objetivos.component';
+import { LineasInvestigacionComponent } from './publico/lineas-investigacion/lineas-investigacion.component';
+import { RutaNoEncontradaComponent } from './publico/ruta-no-encontrada/ruta-no-encontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "contacto",
+    component: ContactoComponent
+  },
+  {
+    path: "objetivos",
+    component: ObjetivosComponent
+  },
+  {
+    path: "lineasInvestigacion",
+    component: LineasInvestigacionComponent
+  },
+  {
+    path:"**",
+    component: RutaNoEncontradaComponent
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './publico/pagina-maestra/encabezado/encabezado.component';
 import { PiePaginaComponent } from './publico/pagina-maestra/pie-pagina/pie-pagina.component';
-import { HomeComponent } from './publico/pagina-maestra/home/home.component';
-import { ObjetivosComponent } from './publico/paginas/objetivos/objetivos.component';
+import { HomeComponent } from './publico/home/home.component';
+import { ObjetivosComponent } from './publico/objetivos/objetivos.component';
+import { ContactoComponent } from './publico/contacto/contacto.component';
+import { PlanEstudiosComponent } from './publico/plan-estudios/plan-estudios.component';
+import { MetodologiaComponent } from './publico/metodologia/metodologia.component';
+import { ProcesoAdmisionComponent } from './publico/proceso-admision/proceso-admision.component';
+import { LineasInvestigacionComponent } from './publico/lineas-investigacion/lineas-investigacion.component';
+import { SeminariosComponent } from './publico/seminarios/seminarios.component';
+import { TesisComponent } from './publico/tesis/tesis.component';
+import { RutaNoEncontradaComponent } from './publico/ruta-no-encontrada/ruta-no-encontrada.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { ObjetivosComponent } from './publico/paginas/objetivos/objetivos.compon
     EncabezadoComponent,
     PiePaginaComponent,
     HomeComponent,
-    ObjetivosComponent
+    ObjetivosComponent,
+    ContactoComponent,
+    PlanEstudiosComponent,
+    MetodologiaComponent,
+    ProcesoAdmisionComponent,
+    LineasInvestigacionComponent,
+    SeminariosComponent,
+    TesisComponent,
+    RutaNoEncontradaComponent,
   ],
   imports: [
     BrowserModule,
